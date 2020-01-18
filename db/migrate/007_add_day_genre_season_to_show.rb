9a713d9997337047213e242f7be129e74cc2985f
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-18 19:47:32 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_genre_season_to_show.rb
+++ db/migrate/007_add_day_genre_season_to_show.rb
@@ -1,11 +1,9 @@
 class AddDayGenreSeasonToShow < ActiveRecord::Migration[5.1]
 
   def change
-    add_column shows: do |t|
-      t.string day:
-      t.string season:
-      t.string genre:
-    end
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+    add_column :shows, :genre, :string
   end
 
 end
