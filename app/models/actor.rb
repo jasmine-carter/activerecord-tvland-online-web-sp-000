a230b2aa88edcab458e15c688cc8fdc3af033364
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-18 19:10:57 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    full_name = []
+    full_name << self.first_name
+    full_name << self.last_name
+    full_name.join("\s")
+  end
+
+  def list_roles
+    #for every character, return its name, then find show name by id
+    a = []
+      self.characters.each do
+      a << "{character.name} - #{Show.find_by(character.show_id).name}"
+    a
+    end
+  end
 end
